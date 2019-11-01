d6e945a1a1d7cde5e9d50e578ab18d5cd8a0d174
Stephen Irons <stephen@brush.co.nz>
2019-11-02 00:05:42 +1300

    New charsets.

---
+++ iconvault_forecastfont_charset.py
@@ -0,0 +1,31 @@
+charset = {
+    'icon-night'          : '\uf100',
+    'icon-sunny'          : '\uf101',
+    'icon-frosty'         : '\uf102',
+    'icon-windysnow'      : '\uf103',
+    'icon-showers'        : '\uf104',
+    'icon-basecloud'      : '\uf105',
+    'icon-cloud'          : '\uf106',
+    'icon-rainy'          : '\uf107',
+    'icon-mist'           : '\uf108',
+    'icon-windysnowcloud' : '\uf109',
+    'icon-drizzle'        : '\uf10a',
+    'icon-snowy'          : '\uf10b',
+    'icon-sleet'          : '\uf10c',
+    'icon-moon'           : '\uf10d',
+    'icon-windyrain'      : '\uf10e',
+    'icon-hail'           : '\uf10f',
+    'icon-sunset'         : '\uf110',
+    'icon-windyraincloud' : '\uf111',
+    'icon-sunrise'        : '\uf112',
+    'icon-sun'            : '\uf113',
+    'icon-thunder'        : '\uf114',
+    'icon-windy'          : '\uf115',
+}
+
+codes = charset.values()
+code_string = ''.join(codes)
+#print(wi_symbols)
+#print(wi_codes)
+print(code_string)
+
